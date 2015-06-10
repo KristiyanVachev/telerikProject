e2c5aa30c72cf611ea127bcf00220295a1a84a8e
Kristiyan Vachev <typhon04@gmail.com>
2015-06-09 21:13:17 -0700

    First commit

---
+++ QuizArena/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizArena.Startup))]
+namespace QuizArena
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
